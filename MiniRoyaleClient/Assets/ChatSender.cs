c29433b09a286c13c47a53aac3608e5cb0d0d9a8
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-03-05 19:38:06 +0300

    UDP implemented
    resource:
    https://www.youtube.com/watch?v=MW91_l2dnnU
    
    https://github.com/hardlydifficult/MultiplayerGameProgramming

---
+++ MiniRoyaleClient/Assets/ChatSender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatSender : MonoBehaviour {
+
+	public UDPConnection udp;
+
+	public string tosend;
+	public bool send = false;
+
+
+	void Update(){
+		if (send) {
+			send = false;
+			udp.Send (tosend);
+		}
+	
+	}
+
+
+}
---
+++ MiniRoyaleClient/Assets/UDPConnection.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class UDPConnection : MonoBehaviour
+{
+	#region Data
+	public ConnectionSettings settings;
+
+	/// <summary>
+	/// IP for clients to connect to. Null if you are the server.
+	/// </summary>
+	public IPAddress serverIp;
+
+	IPEndPoint serverEndPoint;
+
+	UdpClient connection;
+	#endregion
+
+	#region Unity Events
+	public void Awake()
+	{
+		serverIp = IPAddress.Parse (settings.serverAdress);
+		serverEndPoint = new IPEndPoint (serverIp, settings.port);
+
+		connection = new UdpClient (settings.port);
+		connection.BeginReceive(OnReceive, null);
+	}
+
+	private void OnApplicationQuit()
+	{
+		connection.Close();
+	}
+	#endregion
+
+	#region API
+	public void Send(string message)
+	{
+		byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+		connection.Send(data, data.Length, serverEndPoint);
+	}
+
+	void OnReceive(IAsyncResult ar)
+	{
+		try
+		{
+			IPEndPoint ipEndpoint = null;
+			byte[] data = connection.EndReceive(ar, ref ipEndpoint);
+
+			string message = System.Text.Encoding.UTF8.GetString(data);
+
+			MessageReceived(message);
+		}
+		catch(SocketException e)
+		{
+			// This happens when a client disconnects, as we fail to send to that port.
+		}
+		connection.BeginReceive(OnReceive, null);
+	}
+
+	internal void Send(string message, IPEndPoint ipEndpoint)
+	{
+		byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+		connection.Send(data, data.Length, ipEndpoint);
+	}
+
+	public void MessageReceived(string message)
+	{
+		Debug.Log (message);
+	}
+	#endregion
+}
+
