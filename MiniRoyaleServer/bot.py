a45cc060739f3b1b68c2719cec227a22a16b86dc
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-10-03 21:15:29 +0300

    Added bot arhitecture

---
+++ MiniRoyaleServer/bot.py
@@ -0,0 +1,28 @@
+
+
+
+class Bot:
+
+    def __init__(self):
+        self.player = None
+        
+        pass
+
+    def step(self):
+
+        # get positions of near rivals (try to kill without being killed [try to kill if hp>50])
+        # get position of safezone (try to stay in)
+        # get position of near ammo (collect em [if ammo is short])
+        # get position of near obstacles (dont get near them)
+
+        # try to go into safezone
+        # try to shoot at rivals
+        # try to get closer to rivals if health > 50
+        # do otherwise
+        # collect ammo
+
+
+        pass
+
+
+
